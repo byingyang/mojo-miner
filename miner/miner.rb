4af48a42491068b5254cfca3c7a4969729c16700
Brad Yinger <brad@bradyinger.com>
2013-04-25 22:36:52 -0400

    added mojo base boilerplate.

---
+++ miner/miner.rb
@@ -0,0 +1,6 @@
+user = "user"
+password = "password"
+host = "host"
+port = "port"
+serial_port = "COM3"
+rate = 5
---
+++ src/mojo_top.v
@@ -0,0 +1,26 @@
+module mojo_top(
+    input clk,
+    input rst_n,
+    input cclk,
+    output[7:0]led,
+    output spi_miso,
+    input spi_ss,
+    input spi_mosi,
+    input spi_sck,
+    output [3:0] spi_channel,
+    input avr_tx,
+    output avr_rx,
+    input avr_rx_busy,
+	 input button
+    );
+
+wire rst = ~rst_n;
+
+assign spi_miso = 1'bz;
+assign avr_rx = 1'bz;
+assign spi_channel = 4'bzzzz;
+	 
+assign led[7:1] = 7'b0;
+assign led[0] = button;
+
+endmodule
